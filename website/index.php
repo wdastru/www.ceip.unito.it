40517e47dcde822adca7692af91e11268c24c70f
Walter Dastru <walter.dastru@gmail.com>
2013-10-17 13:15:48 +0200

    ...

--- website/index.php
+++ website/index.php
@@ -1,38 +1,26 @@
 <?php
 $localizer = "./";
 ?>
+
 <!DOCTYPE html>
-<html>
-    <!-- InstanceBegin template="/Templates/home.dwt" codeOutsidephpIsLocked="false" -->
-    <head>
-        <meta charset="UTF-8">
-        <!-- InstanceBeginEditable name="title" -->
-        <title>Center of Preclinical Imaging - University of Torino</title>
-        <!-- InstanceEndEditable -->
-        <!-- InstanceBeginEditable name="meta description" -->
-        <meta name="description"
-        content="University of Torino. Website of the Center of Preclinical Imaging." />
-        <!-- InstanceEndEditable -->
-        <meta name="google-site-verification" content="q6BWA8Ypxz6kFHheH3kr6xrFvLXopTTQNMlEEV3vGF0" />
-        <meta name="keywords"
-        content="NMR, MRI, Imaging Molecolare, Molecular Imaging, Molecular, Imaging, Hyperpolarization, Liposomes, Targeting, Contrast Agents, Positron Emission Tomography, PET, microPET, Diagnostic Imaging, Optical Imaging, CEIP, Torino, Italy, Liposomi, risonanza, magnetica, Italia" />
-        <link href="stylesheet.css" rel='stylesheet' type='text/css' />
-        <script type="text/javascript" src="script.js"></script>
-        <?php include_once($localizer . 'includes/analyticstracking.php') ?>
-    </head>
-    
-    <body>
+<head>
+    <title></title>
+    <link href="stylesheet.css" rel='stylesheet' type='text/css' />
+</head>
+<body>
+    <nav>
+        <?php
+            require ($localizer . 'includes/main-nav.php'); 
+        ?>
+    </nav>
+    <article>  
         <div class="repeatingBgImage">
             <div class="BgImage1">
                 <div class="BgImage2">
                     <div id='section0'>
-                        <?php
-                        require ($localizer . 'includes/main-nav.php');
-                        ?>
+                        
                         <div id='header'>CENTER FOR PRECLINICAL IMAGING</div>
-                        <!-- InstanceBeginEditable name="subsection opening" -->
                         <div id="subsection0">
-                            <!-- InstanceEndEditable -->
                             <div id='sidebar'>
                                 <div class='padding'>
                                     <?php
@@ -51,23 +39,52 @@ $localizer = "./";
                                     </div>
                                 </div>
                             </div>
-                            <!-- subsection closing -->
                         </div>
-                        <!-- subsection closing -->
-                        <div id='after'></div>
-                        <?php require ($localizer . 'includes/footer.php'); ?>
-                        <?php require ($localizer . 'includes/validators.php'); ?>
-                        <!-- section closing -->
                     </div>
                 </div>
             </div>
         </div>
-        <!-- section closing -->
+                               
+    </article>
+    <footer>
+        <?php
+            require ($localizer . 'includes/footer.php');
+        ?>
+    </footer>
+</body>
+</html>
+
+
+<!--
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Center of Preclinical Imaging - University of Torino</title>
+        <meta name="description"
+        content="University of Torino. Website of the Center of Preclinical Imaging." />
+        <meta name="google-site-verification" content="q6BWA8Ypxz6kFHheH3kr6xrFvLXopTTQNMlEEV3vGF0" />
+        <meta name="keywords"
+        content="NMR, MRI, Imaging Molecolare, Molecular Imaging, Molecular, Imaging, Hyperpolarization, Liposomes, Targeting, Contrast Agents, Positron Emission Tomography, PET, microPET, Diagnostic Imaging, Optical Imaging, CEIP, Torino, Italy, Liposomi, risonanza, magnetica, Italia" />
+        <link href="stylesheet.css" rel='stylesheet' type='text/css' />
+        <script type="text/javascript" src="script.js"></script>
+        <?php include_once($localizer . 'includes/analyticstracking.php') ?>
+    </head>
+    
+    <body>
+        <div class="repeatingBgImage">
+            <div class="BgImage1">
+                <div class="BgImage2">
+                    
+                </div>
+            </div>
+        </div>
         <script type="text/javascript">
             //<![CDATA[
             changeSideNavStyles();
             //]]>
         </script>
-        <!-- InstanceEnd -->
     </body>
-</html>
+</html> 
+-->
--- website/prova_sticky_footer.php
+++ website/prova_sticky_footer.php
@@ -1,84 +1,57 @@
+
 <?php
 $localizer = "./";
 ?>
 <!DOCTYPE html>
 <html>
-<head>
-    <!-- InstanceBeginEditable name="title" -->
-    <title>Center of Preclinical Imaging - University of Torino</title>
-    <!-- InstanceEndEditable -->
-    <!-- InstanceBeginEditable name="meta description" -->
-    <meta name="description"
-    content="University of Torino. Website of the Center of Preclinical Imaging." />
-    <!-- InstanceEndEditable -->
-    <meta name="google-site-verification"
-    content="q6BWA8Ypxz6kFHheH3kr6xrFvLXopTTQNMlEEV3vGF0" />
+    <head>
+        <title>HTML5 CSS Sticky Footer</title>
 
-    <meta charset="UTF-8">
+        <meta name="description" content="The original and simplest sticky footer CSS modified for HTML5.">
 
-    <meta name="keywords"
-    content="NMR, MRI, Imaging Molecolare, Molecular Imaging, Molecular, Imaging, Hyperpolarization, Liposomes, Targeting, Contrast Agents, Positron Emission Tomography, PET, microPET, Diagnostic Imaging, Optical Imaging, CEIP, Torino, Italy, Liposomi, risonanza, magnetica, Italia" />
-    <!-- <link href="stylesheet.css" rel='stylesheet' type='text/css' /> -->
-    <link href="ceip_style.css" rel='stylesheet' type='text/css' />
-    <script type="text/javascript" src="script.js"></script>
-</head>
+        <!--
+        The second stylesheet is to make things look pretty.
+        The first one is only the important one.
+        -->
 
-<body>
-    <?php include_once($localizer . 'includes/analyticstracking.php') ?>
+        <link rel="stylesheet" type="text/css" media="screen" href="layout.css">
+        <link rel="stylesheet" type="text/css" media="screen" href="style.css">
 
-    <div class="repeatingBgImage">
-        <div class="BgImage1">
-            <div class="BgImage2">
-                <div class="page-wrap">
-                    <div id='section0'>
-                        <?php require ($localizer . 'includes/main-nav.php'); ?>
-                        <div id='header'>
-                            CENTER FOR PRECLINICAL IMAGING
-                        </div>
-                        <!-- InstanceBeginEditable name="subsection opening" -->
-                        <div id="subsection0">
-                            <!-- InstanceEndEditable -->
-                            <div id='sidebar'>
-                                <div class='padding'>
-                                    <?php require ($localizer . 'includes/home_sidebar.php'); ?>
-                                </div>
-                            </div>
-                            <div id='content'>
-                                <div class='paddingOuter'>
-                                    <div class='paddingInner'>
-                                        <p>
-                                            CEIP(Center of Exellence in Preclinical Imaging) offers to universities, research centers and companies
-                                            know-how and methodologies based on imaging techniques in order to easy and speed up the process of
-                                            development of new diagnostic and therapeutics solutions.
-                                        </p>
-                                    </div>
-                                </div>
-                            </div>
-                            <!-- subsection closing -->
-                        </div>
-                        <!-- subsection closing -->
-                        <!--
-                        <div id='after'></div>
-                        <?php require ($localizer . 'includes/footer.php'); ?>
-                        <?php require ($localizer . 'includes/validators.php'); ?>
-                        -->
-                        <!-- section closing -->
-                    </div>
-                </div>
-                <footer class="site-footer">
-                    I'm the Sticky Footer.
-                </footer>
-            </div>
+        <!--
+        Include the following JavaScript file so older browsers can render the HTML5 elements properly
+        -->
+
+        <script type="text/javascript" src="js/html5.js"></script>
+
+    </head>
+    <body>
+
+        <div class="wrapper">
+            
+            <nav>
+                <?php
+                    require ($localizer . 'includes/main-nav.php');
+                ?>
+            </nav>
+
+            <header>
+                
+            </header>
+
+            <article>
+                content
+            </article>
+
+            <div class="push"></div>
+
         </div>
-    </div>
+
+        <footer>
+            <?php
+                require ($localizer . 'includes/footer.php');
+            ?>
+        </footer>
 
 
-    <!-- section closing -->
-    <script type="text/javascript">
-        //<![CDATA[
-        changeSideNavStyles();
-        //]]>
-    </script>
-    <!-- InstanceEnd -->
-</body>
+    </body>
 </html>
